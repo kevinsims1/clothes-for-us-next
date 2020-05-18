7046632b8c4eb494d0dbb5f9afac575e4a77b95b
kevinsims1 <kevindsims1@gmail.com>
2020-05-17 20:25:34 -0700

    styles donate button

--- pages/index.js
+++ pages/index.js
@@ -50,14 +50,25 @@ const Home = () => {
 
       <style jsx>{`
         .donateBtn{
-          background-color: red;
-          cursor: pointer;
-          margin: 1em
+           display:inline-block;
+           padding:0.35em 1.2em;
+           border:0.1em solid #FFFFFF;
+           margin:0 0.3em 0.3em 0;
+           border-radius:0.12em;
+           box-sizing: border-box;
+           text-decoration:none;
+           font-family:'Roboto',sans-serif;
+           font-weight:300;
+           background-color: black;
+           text-align:center;
+           transition: all 0.2s;
+           color: #fff
         }
 
         .donateBtn:hover{
-          background-color: black;
-          color: white
+          border-color: red;
+          background-color:#fff;
+          color: black
         }
         .section{
           margin: 4em;
