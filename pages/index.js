091b85b8b5ac05276d4b219edb3d0c2b3147d20b
kevinsims1 <kevindsims1@gmail.com>
2020-05-24 19:24:05 -0700

    nav working

--- pages/index.js
+++ pages/index.js
@@ -9,147 +9,147 @@ const Home = () => {
     setToggle(!toggle)
   }
 
-  useEffect(() => {
-    w = window.innerWidth
-    console.log(w)
-    if(w > 760){
-      setToggle(true)
-    }
-    
-  }, []);
+  // useEffect(() => {
+  //   w = window.innerWidth
+  //   console.log(w)
+  //   if (w > 760) {
+  //     setToggle(true)
+  //   }
+  // }, [])
 
-  const handleResize = () => {
-    setResize(true)
-  }
+  // const handleResize = () => {
+  //   setResize(true)
+  // }
   return (
-    <div className="container" >
+    <div className="container">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <header className="header">
         <img src="/logo-4.svg" className="logo" />
-        <div style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          flexDirection: 'column',
-          position: 'relative'
-        }}>
-          <div className='dropDown'>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            position: 'relative',
+          }}
+        >
+          {/* desktop */}
+          <div className="desktopNav">
+            <nav id="desktopHomeNav">
+                <ul>ABOUT US</ul>
+                <ul>LEARN MORE</ul>
+                <ul>DONATE NOW!</ul>
+            </nav>
+          </div>
+          {/* mobile */}
+          <div className="mobileNav">
             <button onClick={Toggle}>
               <img src="/ham.svg" className="burger" />
             </button>
-            {toggle &&  <nav id="homeNav">
-              <ul>ABOUT US</ul>
-              <ul>LEARN MORE</ul>
-              <ul>DONATE NOW!</ul>
-            </nav>}
+            {toggle && (
+              <nav id="homeNav">
+                <ul>ABOUT US</ul>
+                <ul>LEARN MORE</ul>
+                <ul>DONATE NOW!</ul>
+              </nav>
+            )}
           </div>
         </div>
       </header>
 
-
       <main>
-        <div className='section'>
-          <h1>
-            Work for good, donate with a purpose
-          </h1>
+        <div className="section">
+          <h1>Work for good, donate with a purpose</h1>
           <p>
-            Go from passion project to nonprofit and get 
-            started the right way. Become eligible to file 
-            for 501(c)(3) tax-exempt status and more.
+            Go from passion project to nonprofit and get started the right way.
+            Become eligible to file for 501(c)(3) tax-exempt status and more.
           </p>
-          <button className='donateBtn'>
-            DONATE NOW
-          </button>
+          <button className="donateBtn">DONATE NOW</button>
         </div>
-        <div className='section'>
-          <h1>
-            Apply for clothing assistance here
-          </h1>
+        <div className="section">
+          <h1>Apply for clothing assistance here</h1>
           <p>
-            Do you or your children need clothes? 
-            Apply now and if accepted we will send you
-            a years worth of clothing.
+            Do you or your children need clothes? Apply now and if accepted we
+            will send you a years worth of clothing.
           </p>
-          <button className='applyBtn'>
-            APPLY NOW
-          </button>
+          <button className="applyBtn">APPLY NOW</button>
         </div>
-        <div className='section'>
-          <h1>
-            Work for good, donate with a purpose
-          </h1>
+        <div className="section">
+          <h1>Work for good, donate with a purpose</h1>
           <p>
-            Go from passion project to nonprofit and get 
-            started the right way. Become eligible to file 
-            for 501(c)(3) tax-exempt status and more.
+            Go from passion project to nonprofit and get started the right way.
+            Become eligible to file for 501(c)(3) tax-exempt status and more.
           </p>
-          <button className='donateBtn'>
-            DONATE NOW
-          </button>
+          <button className="donateBtn">DONATE NOW</button>
         </div>
       </main>
 
       <style jsx>{`
-        .applyBtn, .donateBtn{
-           padding:0.35em 1.2em;
+        .applyBtn,
+        .donateBtn {
+           padding: 0.35em 1.2em;
            margin: 1em;
-           text-decoration:none;
-           border-radius: 2em;
-           text-align:center;
+           text-decoration: none;
+          border-radius: 2em;
+           text-align: center;
            transition: all 0.2s;
-           color: #fff;
+          color: #fff;
         }
 
-        .donateBtn{
+        .donateBtn {
            background-color: red;
         }
 
-        .applyBtn{
+        .applyBtn {
            background-color: black;
         }
 
-        .applyBtn:hover{
+        .applyBtn:hover {
           border-color: black;
-          background-color:red;
-          color: #fff
+          background-color: red;
+          color: #fff;
         }
 
-        .donateBtn:hover{
+        .donateBtn:hover {
           border-color: red;
-          background-color:black;
-          color: #fff
+          background-color: black;
+          color: #fff;
         }
 
-        .section{
+        .section {
           margin: 3em 5em;
           display: flex;
           flex-direction: column;
           align-items: center;
         }
 
-        h1{
+        h1 {
           font-size: 28px;
           text-align: center;
           font-weight: normal;
-          margin: .5em;
-          line-height: 1.4
+          margin: 0.5em;
+          line-height: 1.4;
         }
 
-        p{
+        p {
           text-align: center;
-          margin: .5em 1.2em;
-          line-height: 1.6
+          margin: 0.5em 1.2em;
+          line-height: 1.6;
         }
 
-        .dropDown{
+        .mobileNav {
           float: right;
           position: relative;
           display: inline-block;
         }
 
         button {
-          margin: .5em;
+          margin: 0.5em;
           padding: 0;
           border: none;
           background-color: none;
@@ -162,16 +162,20 @@ const Home = () => {
           max-width: 100px;
         }
 
-        #homeNav{
+        #homeNav {
           position: absolute;
           background-color: #f1f1f1;
           min-width: 160px;
           overflow: auto;
-          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+          box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
           right: 0;
           z-index: 1;
         }
-      
+
+        .desktopNav{
+          display: none !important
+        }
+
         .burger {
           display: inline;
           height: 30px;
@@ -183,11 +187,16 @@ const Home = () => {
         }
 
         @media only screen and (min-width: 760px) {
-          .burger {
+          .mobileNav {
             display: none;
           }
 
-          #homeNav {
+          .desktopNav {
+            display: block !important;
+            width: 100% !important;
+          }
+          
+          #desktopHomeNav {
             display: flex !important;
             flex-direction: row !important;
             align-items: center !important;
